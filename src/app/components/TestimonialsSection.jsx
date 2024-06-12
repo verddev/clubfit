74e9874a54b8a41e4284820008bf6f2a103fea5e
verddev <stuka1332@gmail.com>
2024-06-12 01:33:05 -0300

    Update TestimonialsSection.jsx

--- src/app/components/TestimonialsSection.jsx
+++ src/app/components/TestimonialsSection.jsx
@@ -7,7 +7,7 @@ const TestimonialsSection = () => (
     <h2>Depoimentos</h2>
     <div className={styles.testimonialCards}>
       <div className={styles.testimonialCard}>
-        <p>Simplesmente incrível! As dicas são práticas e os alimentos são fáceis de encontrar. Estou vendo resultados em poucas semanas. - Hellena Silva</p>
+        <p>"Simplesmente incrível! As dicas são práticas e os alimentos são fáceis de encontrar. Estou vendo resultados em poucas semanas." - Hellena Silva</p>
       </div>
       <div className={styles.testimonialCard}>
         <Image
@@ -17,10 +17,10 @@ const TestimonialsSection = () => (
               height={200}
               style={styles.imgResult}/>
         
-        <p>Este guia transformou minha alimentação e meus resultados na academia! Recomendo a todos que querem ganhar massa muscular sem gastar muito. - Felipe Doudement</p>
+        <p>"Este guia transformou minha alimentação e meus resultados na academia! Recomendo a todos que querem ganhar massa muscular sem gastar muito". - Felipe Gabriel</p>
       </div>
       <div className={styles.testimonialCard}>
-        <p>Nunca pensei que seria possível ganhar músculos com uma dieta tão acessível. Este guia é um divisor de águas! - Pedro Sabino</p>
+        <p>"Nunca pensei que seria possível ganhar músculos com uma dieta tão acessível. Este guia é um divisor de águas!" - Alison Galdino</p>
       </div>
     </div>
   </section>
