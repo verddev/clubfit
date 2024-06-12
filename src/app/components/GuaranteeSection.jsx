21c35a48499a3807eb6fd93025e6cae0a24321a4
verddev <stuka1332@gmail.com>
2024-06-12 00:39:46 -0300

    add link

--- src/app/components/GuaranteeSection.jsx
+++ src/app/components/GuaranteeSection.jsx
@@ -5,7 +5,7 @@ const GuaranteeSection = () => (
   <section className={styles.guarantee}>
     <h2>Satisfação Garantida</h2>
     <p>Se você não estiver satisfeito com os resultados, 7 dias para devolvemos seu dinheiro. Sem perguntas, sem complicações.</p>
-    <a target='_blank' rel="noreferrer" href="https://pay.kiwify.com.br/suNMS15" className={`${styles.btn} ${styles['primary-btn']}`}>Compre Agora com Confiança</a>
+    <a target='_blank' rel="noreferrer" href="https://pay.kiwify.com.br/v17fyAA" className={`${styles.btn} ${styles['primary-btn']}`}>Compre Agora com Confiança</a>
   </section>
 );
 
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -5,7 +5,7 @@ const HeroSection = () => (
   <section className={styles.hero}>
     <h1>Ganhe Massa Muscular com Alimentos Caseiros</h1>
     <p>Transforme sua rotina alimentar e alcance seus objetivos musculares sem suplementos caros!</p>
-    <a href="https://pay.kiwify.com.br/suNMS15" rel="noreferrer" target="_blank" className={`${styles.btn} ${styles['primary-btn']}`}>Compre Agora</a>
+    <a href="https://pay.kiwify.com.br/v17fyAA" rel="noreferrer" target="_blank" className={`${styles.btn} ${styles['primary-btn']}`}>Compre Agora</a>
   </section>
 );
 
